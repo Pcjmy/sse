a935acf151c1a5eab87adfa1e8d6baed7388d3d5
Pcjmy <1716045191@qq.com>
2024-02-20 23:50:51 +0800

    feat: stream

--- sse-server/src/app.controller.ts
+++ sse-server/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('stream')
+  stream() {
+    return new Observable((observer) => {
+      observer.next({ data: { msg: 'aaa' } });
+
+      setTimeout(() => {
+        observer.next({ data: { msg: 'bbb' } });
+      }, 2000);
+
+      setTimeout(() => {
+        observer.next({ data: { msg: 'ccc' } });
+      }, 5000);
+    });
   }
 }
